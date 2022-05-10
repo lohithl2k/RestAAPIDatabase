b765177e4881cf99bb1ffb67cf7b0d6128cc864e
VIS <VIS@LAPTOP-LJA66PGL>
2022-05-10 17:34:35 +0530

    WithoutConDB

---
+++ EmpDatabaseRestAPI/src/main/java/employee/Employees.java
@@ -0,0 +1,44 @@
+package employee;
+
+public class Employees {
+	
+	private int empID;
+	private String empName;
+	private String empAge;
+	private String empDept;
+	private String empEmail;
+	
+	public int getEmpID() {
+		return empID;
+	}
+	public void setEmpID(int empID) {
+		this.empID = empID;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getEmpAge() {
+		return empAge;
+	}
+	public void setEmpAge(String empAge) {
+		this.empAge = empAge;
+	}
+	public String getEmpDept() {
+		return empDept;
+	}
+	public void setEmpDept(String empDept) {
+		this.empDept = empDept;
+	}
+	public String getEmpEmail() {
+		return empEmail;
+	}
+	public void setEmpEmail(String empEmail) {
+		this.empEmail = empEmail;
+	}
+	
+	
+	
+}
